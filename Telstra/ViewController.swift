1d7e2cdb40275726f4b1babed8c0a1872f5a9c0e
Rui Jia <jiarui.dev@gmail.com>
2020-04-22 17:28:50 +1000

    Init commit

---
+++ Telstra/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Telstra
+//
+//  Created by ruixue on 22/4/20.
+//  Copyright © 2020 jiarui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
